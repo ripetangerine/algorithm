19cd35cb1147e09d3c3f0327470a020c9c4c9339
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-03-30 17:10:39 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    // A+B, A-B, A*B, A/B(몫), A%B(나머지)
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    
+    return 0;
+}
