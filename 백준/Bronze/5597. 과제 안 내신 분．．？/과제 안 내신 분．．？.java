197244f7f274dc9537100f6e9c5c04426e0d35a0
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-05-12 22:52:18 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,24 +1,20 @@
 import java.util.*;
-
 public class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[31];
+        
+        
+        for(int i=1; i<29; i++){
+            int num = in.nextInt();
+            arr[num] = 1;
+        }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] student = new int[31];
-		
-		for(int i=1; i<29; i++) {
-			int success = sc.nextInt();
-			student[success] = 1;
-		}
-		for(int i=1; i<student.length; i++) {
-			if(student[i]!=1)
-				System.out.println(i);
-		}
-		
-		
-		sc.close();
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]!=1)
+                System.out.println(i);
+        }
+        in.close();
 
-	}
-
+    }
 }
-
