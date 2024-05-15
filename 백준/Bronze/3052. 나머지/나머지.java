a5bd8b2528a1a39c1721051e8d602467a2bbb836
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-05-15 09:23:30 +0900

    [Bronze II] Title: 나머지, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
 import java.util.HashSet;
+import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
-        HashSet<Integer> h = new HashSet<Integer>();
+        HashSet <Integer> h = new HashSet <Integer>();
         
         for(int i=0; i<10; i++){
-            h.add(in.nextInt() % 42);
+            h.add(in.nextInt()%42);
         }
-        in.close();
         System.out.println(h.size());
+        
     }
 }
