dd6002308b65d6f0bf66dc7a8ba7cb54a8222bc0
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-05-15 10:37:04 +0900

    [Bronze I] Title: 평균, Time: 252 ms, Memory: 18780 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,20 +3,22 @@ public class Main{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        double add=0,max;
-        double arr[]= new double[N];
+        int max;
+        double aver,add=0;
+        int arr[]= new int[N];
         
         for(int i=0; i<N; i++){
-            arr[i]=in.nextDouble();
+            arr[i]=in.nextInt();
         }
         max = arr[0];
         for(int i=1; i<N; i++){
             if(arr[i]>max) max = arr[i];
         }
         for(int i=0; i<N; i++){
-            add += (arr[i]/max)*100;
+            add += ((double)arr[i]/max)*100;
         }
-        System.out.println(add/N);
+        aver = add/N;
+        System.out.println(aver);
         
     }
 }
