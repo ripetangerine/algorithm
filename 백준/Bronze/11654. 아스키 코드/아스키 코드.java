82f817b4605cb2aaeae273f8a2f7034c40669719
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-05-15 11:50:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int ch = in.next().charAt(0);
+        System.out.print(ch);
+    }
+}
