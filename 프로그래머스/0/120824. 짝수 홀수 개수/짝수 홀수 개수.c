dc4c2d6617da0dec1467e42c06c79766039e3ea8
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-05-20 22:26:20 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int odd=0,evn=0;
+    int* answer = (int*)malloc(2);
+    for(int i=0; i<num_list_len; i++){
+        if(num_list[i]%2==0) evn ++;
+        else odd++;
+    }
+    answer[0]=evn;
+    answer[1]=odd;
+    
+    return answer;
+    return 0;
+}
