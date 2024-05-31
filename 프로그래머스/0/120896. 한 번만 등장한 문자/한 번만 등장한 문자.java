771ff409c51ea0914200e2a9a43026467a95ccc5
buddybuddy <165144647+KwakJakyeong@users.noreply.github.com>
2024-06-01 01:21:25 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.54 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String arr[] = s.split("");
+        int cnt = 0;
+        Arrays.sort(arr); // abcabcadc abcabcadc
+        for(int i=0; i<arr.length; i++){
+            cnt = 0;
+            for(int j=0; j<arr.length; j++){
+                if(arr[i].equals(arr[j])){
+                    cnt++;
+                }
+            }
+            if(cnt==1){
+                answer += arr[i];
+            }
+        }
+        return answer; 
+    }
+}
