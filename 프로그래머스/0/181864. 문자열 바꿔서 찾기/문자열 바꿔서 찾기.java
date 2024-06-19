3e508cf3643451706a60e14dadefad3f5e8075e3
buddybuddy0101 <165144647+KwakJakyeong@users.noreply.github.com>
2024-06-19 15:41:02 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 2.27 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        String change = "";
+        for(int i=0; i<myString.length(); i++){
+            if(myString.charAt(i)=='A')
+                change += "B";
+            else 
+                change += "A";
+        }
+        return (change.contains(pat))?1:0;
+    }
+}
