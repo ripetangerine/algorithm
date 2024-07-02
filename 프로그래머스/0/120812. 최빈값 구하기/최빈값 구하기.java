a7064470cc7acdb1479b0185ade5d3c9f85f7c7b
LegendBlueTangerine <165144647+KwakJakyeong@users.noreply.github.com>
2024-07-02 19:31:05 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.04 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,22 +1,28 @@
-import java.util.Arrays;
+import java.util.Arrays; 
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array);
-        int max = array[array.length-1]; //{1,2,3,3,4} max = 4; {1,3,4,9}
-        int cnt[] = new int[max+1]; // cnt의 길이는 5; cnt 길이는 10;
-        for(int i=0; i<array.length; i++){ //array의 길이는 5; / 4;
-            cnt[array[i]]++; 
+        int mx = array[0];
+        for(int i=0; i<array.length; i++){
+            if(mx<array[i])
+                mx = array[i];
         }
-        max = cnt[0];
-        int answer = 0;
-        for(int i=1; i<cnt.length; i++){
-            if(max<cnt[i]){
-                max = cnt[i];
-                answer = i;
+        
+        int cnt[] = new int[mx+1];
+        
+        for(int i=0; i<array.length; i++){
+            cnt[array[i]]++;
+        }
+        
+        int index = 0; 
+        mx = 0;
+        for(int i=0; i<cnt.length; i++){
+            if(mx < cnt[i]){
+                index = i;
+                mx = cnt[i];
             }
-            else if(max==cnt[i])
-                answer = -1;
+            else if(mx == cnt[i])
+                index = -1;
         }
-        return answer; 
+        return index; 
     }
 }
