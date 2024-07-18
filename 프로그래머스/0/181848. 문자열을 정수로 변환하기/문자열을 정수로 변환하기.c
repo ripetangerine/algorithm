dc071461e32cfbd9f9a84ff9059b223b324c0659
LegendBlueTangerine <165144647+KwakJakyeong@users.noreply.github.com>
2024-07-18 16:03:06 +0900

    [level 0] Title: 문자열을 정수로 변환하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.c
+++ 프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* n_str) {
-    return atoi(n_str);
+    int* num = (int*)malloc(strlen(n_str)*sizeof(int));
+    sscanf(n_str,"%d",num);
+    return num;
 }
