54de6680d1834c8774e7ff56953482e207213902
KwakJakyeong1 <165144647+KwakJakyeong@users.noreply.github.com>
2024-08-31 05:24:17 +0900

    [level 0] Title: 2의 영역, Time: 4.69 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<arr.length; i++)
+            if(arr[i]==2) list.add(i);
+        
+        if(list.size()==0) return new int[]{-1};
+        else return Arrays.copyOfRange(arr,list.get(0),list.get(list.size()-1)+1);
+        
+    }
+}
