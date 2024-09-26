f70639358e41bed44f4939da633cd76bea024233
KwakJakyeong1 <165144647+KwakJakyeong@users.noreply.github.com>
2024-09-26 13:02:37 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.10 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -9,7 +9,8 @@ class Solution {
         for(int i=0; i<arr.length; i++){
             if(!set.contains(arr[i])){
                 set.add(arr[i]);
-                answer[cnt++] = arr[i];
+                answer[cnt] = arr[i];
+                cnt++;
             }
             if(cnt == k) break;
         }
