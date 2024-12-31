6f63490260312299d86650aeae7743590a6efce9
KwakJakyeong <165144647+KwakJakyeong@users.noreply.github.com>
2024-12-31 23:09:45 +0900

    [level 0] Title: 배열 조각하기, Time: 1.40 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,14 @@
+function solution(arr, query) {
+    var answer = [];
+    
+    for(let i=0; i<query.length; i++){
+        if(i%2===0){
+            arr.splice(query[i]+1, arr.length - (query[i]+1));
+            
+        }else{
+            arr.splice(0, query[i]);
+        }
+    }
+    
+    return arr;
+}
