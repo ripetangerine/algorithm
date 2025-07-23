b8af434cba2414d5f0b1c18c2655211d68e45810
KwakJagyeong <ggyul847@gmail.com>
2025-07-23 10:37:57 +0900

    [Bronze II] Title: 분해합, Time: 1156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,8 +1,7 @@
 n = int(input())
 answer = 0
 for i in range(1, n+1):
-    nums = list(map(int, str(i)))
-    answer = sum(nums) + i
+    answer = sum(list(map(int, str(i)))) + i
     if answer == n:
         print(i)
         break
